e09cec957d5211dbca9ba3bfd2bbc0147e5284d2
ARTURO RIOS <arturo.rioscueto@gmail.com>
2022-01-13 21:26:52 -0600

    se añaden detalles de responsividad

--- assets/getposts.js
+++ assets/getposts.js
@@ -1,5 +1,5 @@
-let template = ""
-let indice = 0
+let template = "";
+let indice = 0;
 const getposts = (callback) => {
   fetch(
     "https://desafio-front-end-ea066-default-rtdb.firebaseio.com/posts/.json"
@@ -13,67 +13,68 @@ const getposts = (callback) => {
 };
 
 const callBack = (posts) => {
-  let post = Object.entries(posts).reverse()
-    for (i = indice; i <= indice + 5; i++) {
-        template += `
+  let post = Object.entries(posts).reverse();
+  for (i = indice; i <= indice + 5; i++) {
+    template += `
        
              <div class='my-2 d-flex cards mt-4'>
-             <div class='col-md-9 col-sm-8'>
-              <p>
-              <img
-                      class="anc imagu"
-                      src="${post[i][1].imagenPost}"
-                      alt
+             <div class='col-md-6 col-lg-8 col-xl-9'>
+                <p>
+                  <img
+                          class="anc imagu"
+                          src="${post[i][1].imagenPost}"
+                          alt
+                        >
+                    <a class="text-slow fw-bold text-decoration-none text-dark" href=""
+                      >${post[i][1].autor}</a>
+                    
+                    <span class="text-muted">in</span
+                    ><a class="fw-bold text-decoration-none text-dark" href="">
+                      Medium</a
                     >
-                <a class="text-slow fw-bold text-decoration-none text-dark" href=""
-                  >${post[i][1].autor}</a>
-                
-                <span class="text-muted">in</span
-                ><a class="fw-bold text-decoration-none text-dark" href="">
-                  Medium</a
-                >
-                · <span class="text-muted">${post[i][1].fechaCreacion}</span>
-              </p>
-              <a class="title text-justify text-decoration-none text-dark fw-bold" id="tilte href="listpost.html?idpost=${post[i][0]}"
-                >${post[i][1].titulo}</a
-              >
-              <a class ="text-decoration-none" href='listpost.html?idpost=${post[i][0]}'>
-              <p class="card-text text-muted fs-6 abstract">
-              ${post[i][1].abstract}
-              </p>
-              </a>
-              <p class="card-text">
-                <small class="text-muted"
-                  ><span> ${post[i][1].tiempoLectura} </span
-                  ><button class="fondo border-0 text-muted">
-                    Working Class</button
-                  ><span> · Popular on Medium</span>
-                </small><button class="btnfn border-0 ms-4"></button>
-              </p>
-              </div>
+                    · <span class="text-muted">${post[i][1].fechaCreacion}</span>
+                  </p>
+                  <a class="title text-justify text-decoration-none text-dark fw-bold" id="tilte href="listpost.html?idpost=${post[i][0]}"
+                    >${post[i][1].titulo}</a
+                  >
+                  <a class ="text-decoration-none" href='listpost.html?idpost=${post[i][0]}'>
+                  <p class="card-text text-muted fs-6 abstract">
+                  ${post[i][1].abstract}
+                  </p>
+                  </a>
+                  <p class="card-text">
+                    <small class="text-muted"
+                      ><span> ${post[i][1].tiempoLectura} </span
+                      ><button class="fondo border-0 text-muted">
+                        Working Class</button
+                      ><span> · Popular on Medium</span>
+                    </small><button class="btnfn border-0 ms-4"></button>
+                  </p>
+                  </div>
 
               <img
-                  class="ms-2 img-fluid img"
+                  class="ms-2 img-fluid imagenPost"
                   src="${post[i][1].imagenPost}"
                   
                   alt=""
                   />
                   </div>
                   
-            `
-            
-    }
-   
-  document.querySelector(".posts").innerHTML = template;
-  indice = document.querySelectorAll('.posts .cards').length-1
+            `;
+  }
 
+  document.querySelector(".posts").innerHTML = template;
+  indice = document.querySelectorAll(".posts .cards").length - 1;
 };
 
 getposts(callBack);
 
 //Aqui se realiza el scroll
-window.addEventListener('scroll',()=>{
-  if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight-1.5){
-    getposts(callBack)
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight - 1.5
+  ) {
+    getposts(callBack);
   }
-})
+});
